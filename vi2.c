5b853ca30a2368eb223a62b60dbc34a95e0cc997
mits-hirayama <mits.hirayama@gmail.com>
2020-04-28 19:07:47 +0900

    add,commitのコマンド追記
    練習のため

---
+++ vi2.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+int main(){}jjkたちつてと
+aiueoアイウエオ
+#include<stdio.h>
+int main(){
+
+printf("hello");
+printf("WAKYO");
+printf("hello");
+printf("hello");
+printf("WAKYO");
+printf("WAKYO");
+printf("hello");
+printf("WAKYO");
+ayasdfkj
+printf("hello");
+
+printf("WAKYO");
+printf("hello");
+printf("WAKYO");
+printf("hello");
+vi2.c
+
+
+}hellA¥aaoaaa
+aaa aaa aa aaa aa`
+vvv
+bbbbbbbb
+asdlfkj
+as;ldkfjs;
+as;ldfkjas;ldkf
+
+asdfas;ldfjasldkfjasasdflkjikjloijI
+;ldkjfalskdjf;lsak
+sldfkj
+helllo world qwerrtla
